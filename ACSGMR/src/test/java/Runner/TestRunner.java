c8e518b9ff13c5b7b8c89662d256849a591e7d11
PratikshaKamble-kls <99633150+PratikshaKamble-kls@users.noreply.github.com>
2023-06-23 15:09:35 +0530

    merged

---
+++ ACSGMR/src/test/java/Runner/TestRunner.java
@@ -0,0 +1,24 @@
+package Runner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions(features = {"Features"}, 
+	                 glue = "stepdefinition",
+	                 monochrome = true,
+	                 plugin = { "json:target/cucumber.json",
+	                		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+	                		 "rerun:Rerun/rerun1.txt" },
+	                 		
+
+	                 tags ="@ACS_GMR_Import")
+
+public class TestRunner {
+		
+}
+
+	
+	
